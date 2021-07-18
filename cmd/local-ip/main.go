0c3371e04feb8beebac84fb9896aa0bacc0e1499
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-07-18 13:43:23 +0300

    stun: extract stun functions to lib

--- cmd/local-ip/main.go
+++ cmd/local-ip/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"encoding/binary"
 	"fmt"
 	"net"
-)
 
-var stunServerName = "stun.l.google.com:19302"
+	"github.com/stormtrooper1859/p2p-chat/internal/stun"
+)
 
 func main() {
 	addr := net.UDPAddr{
@@ -20,53 +18,6 @@ func main() {
 		return
 	}
 
-	dst, err := net.ResolveUDPAddr("udp", stunServerName)
-	if err != nil {
-		fmt.Printf("ResolveUDPAddr error  %v", err)
-		return
-	}
-
-	_, err = ser.WriteToUDP(getData(), dst)
-	if err != nil {
-		fmt.Printf("WriteToUDP error  %v", err)
-		return
-	}
-
-	p := make([]byte, 2048)
-	var n int
-	n, err = bufio.NewReader(ser).Read(p)
-	if err == nil {
-		fmt.Println(p[:n])
-
-		ip := make([]byte, 4)
-		binary.BigEndian.PutUint32(ip, 0x2112A442)
-		start := 20 + 4 + 4
-
-		for i := 0; i < 4; i++ {
-			ip[i] ^= p[start+i]
-		}
-
-		fmt.Println(ip)
-	} else {
-		fmt.Printf("Reader error %v\n", err)
-	}
-	ser.Close()
-}
-
-func getData() []byte {
-	res := make([]byte, 20)
-
-	// query type
-	binary.BigEndian.PutUint16(res[0:], 0x0001)
-	// msg length
-	binary.BigEndian.PutUint16(res[2:], 0x0000)
-	// magic cookie
-	binary.BigEndian.PutUint32(res[4:], 0x2112A442)
-
-	// transaction id
-	for i := 5; i < 20; i++ {
-		res[i] = byte(i)
-	}
-
-	return res
+	ip, port := stun.GetIP(ser)
+	fmt.Printf("%s:%d\n", ip.String(), port)
 }
---
+++ internal/stun/stun.go
@@ -0,0 +1,101 @@
+package stun
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"time"
+)
+
+const (
+	secretKey uint32 = 0x2112A442
+)
+
+var stunServerName = "stun.l.google.com:19302"
+var stunServerAddr *net.UDPAddr
+
+func getData() []byte {
+	res := make([]byte, 20)
+
+	// query type
+	binary.BigEndian.PutUint16(res[0:], 0x0001)
+	// msg length
+	binary.BigEndian.PutUint16(res[2:], 0x0000)
+	// magic cookie
+	binary.BigEndian.PutUint32(res[4:], secretKey)
+
+	// transaction id
+	for i := 5; i < 20; i++ {
+		res[i] = byte(i)
+	}
+
+	return res
+}
+
+func GetIP(conn *net.UDPConn) (resultIp net.IP, port uint16) {
+	var err error
+	stunServerAddr, err = net.ResolveUDPAddr("udp", stunServerName)
+	if err != nil {
+		fmt.Printf("ResolveUDPAddr error  %v", err)
+		return
+	}
+
+	_, err = conn.WriteToUDP(getData(), stunServerAddr)
+	if err != nil {
+		fmt.Printf("WriteToUDP error  %v", err)
+		return
+	}
+
+	responseBuffer := make([]byte, 2048)
+	_, _, err = conn.ReadFromUDP(responseBuffer)
+	ip := make([]byte, 4)
+	if err == nil {
+		// fmt.Println(responseBuffer[:n])
+
+		binary.BigEndian.PutUint32(ip, secretKey)
+		start := 20 + 4 + 4
+
+		for i := 0; i < 4; i++ {
+			ip[i] ^= responseBuffer[start+i]
+		}
+
+		port = binary.BigEndian.Uint16(responseBuffer[start-2:])
+
+		// fmt.Println(ip)
+	} else {
+		fmt.Printf("Reader error %v\n", err)
+		return nil, 0
+	}
+
+	return net.IPv4(ip[0], ip[1], ip[2], ip[3]), port
+}
+
+func getIndicationData() []byte {
+	res := make([]byte, 20)
+
+	// query type
+	binary.BigEndian.PutUint16(res[0:], 0x0010)
+	// msg length
+	binary.BigEndian.PutUint16(res[2:], 0x0000)
+	// magic cookie
+	binary.BigEndian.PutUint32(res[4:], secretKey)
+
+	// transaction id
+	for i := 5; i < 20; i++ {
+		res[i] = byte(i)
+	}
+
+	return res
+}
+
+func SendIndication(conn *net.UDPConn) {
+	data := getIndicationData()
+	for {
+		_, err := conn.WriteToUDP(data, stunServerAddr)
+		if err != nil {
+			fmt.Printf("SendIndication WriteToUDP error  %v", err)
+			return
+		}
+		time.Sleep(250 * time.Millisecond)
+	}
+}
